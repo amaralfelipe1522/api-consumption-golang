9f889f629c092a058a301fdcaf3b6dfa3fc15c1b
amaralfelipe1522 <amaral.felipe@live.com>
2020-03-21 10:42:13 -0300

    Adding some comments

--- main.go
+++ main.go
@@ -10,13 +10,15 @@ import (
 
 func main() {
 	fmt.Println("Starting the application...")
-	response, err := http.Get("https://httpbin.org/ip")
+	// GET example
+	response, err := http.Get("https://httpbin.org/user-agent")
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
 		data, _ := ioutil.ReadAll(response.Body)
 		fmt.Println(string(data))
 	}
+	// POST example
 	jsonData := map[string]string{"firstname": "Felipe", "lastname": "Amaral"}
 	jsonValue, _ := json.Marshal(jsonData)
 	response, err = http.Post("https://httpbin.org/post", "application/json", bytes.NewBuffer(jsonValue))
